ccfc503bfaae217732d9ff4d4623de4050b5a280
zhangx511 <zhangx511@chinaunicom.cn>
2020-08-18 17:40:56 +0800

    Spring Boot实例

--- src/main/java/com/mxixm/spring/boot/chapter02/model/MyModelBinding.java
+++ src/main/java/com/mxixm/spring/boot/chapter02/model/MyModelBinding.java
@@ -11,27 +11,39 @@ import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
+/**
+ * @author zhangx511
+ */
 @Controller
 @SessionAttributes("name")
 public class MyModelBinding {
 
     @RequestMapping(path = "/mapModel")
     public String mapModelBinding(Map<String, Object> map) {
-        // 声明Map类型，此时传入的参数是ExtendedModelMap的实例，操作时使用Map接口的方法
+
+        /**
+         * 声明Map类型，此时传入的参数是ExtendedModelMap的实例，操作时使用Map接口的方法
+         */
         map.put("name", "Map类型的Model");
         return "defaultView";
     }
 
     @RequestMapping(path = "/modelModel")
     public String modelModelBinding(Model model) {
-        // 声明Model类型，使用Model的接口方法操作数据
+
+        /**
+         * 声明Model类型，使用Model的接口方法操作数据
+         */
         model.addAttribute("name", "Model类型的Model");
         return "defaultView";
     }
 
     @RequestMapping(path = "/modelMapModel")
     public String modelMapModelBinding(ModelMap modelMap) {
-        // 绑定ModelMap类型，使用两种方式添加属性
+
+        /**
+         * 绑定ModelMap类型，使用两种方式添加属性
+         */
         modelMap.addAttribute("name", "ModelMap类型的Model");
         modelMap.put("date", new Date());
         return "defaultView";
@@ -40,8 +52,11 @@ public class MyModelBinding {
     @RequestMapping(path = "/defaultView")
     public Map createModel() {
         Map<String, Object> model = new HashMap<>(2);
-        // 这里也可以创建ModelMap类型或者ExtendModelMap类型，都是可以的。
-        // ModelMap model = new ModelMap();
+
+        /**
+         * 这里也可以创建ModelMap类型或者ExtendModelMap类型，都是可以的
+         * ModelMap model = new ModelMap();
+         */
         model.put("name", "自行创建Model");
         return model;
     }
@@ -50,7 +65,10 @@ public class MyModelBinding {
     public ModelAndView modelAndViewModelBinding() {
         Map<String, Object> model = new HashMap<>(2);
         model.put("name", "ModelAndView类型");
-        // 返回ModelAndView，第一个参数是视图名，第二个参数是模型。
+
+        /**
+         * 返回ModelAndView，第一个参数是视图名，第二个参数是模型
+         */
         return new ModelAndView("defaultView", model);
     }
 
