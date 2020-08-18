855cd1b724d6c0a74e787b9a1f98b0f92ee7fe20
zhangx511 <zhangx511@chinaunicom.cn>
2020-08-18 11:12:22 +0800

    Spring Boot实例

---
+++ src/main/java/com/mxixm/spring/boot/chapter03/argument/MultipartArgumentResolveController.java
@@ -0,0 +1,21 @@
+package com.mxixm.spring.boot.chapter03.argument;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.multipart.MultipartFile;
+
+@Controller
+public class MultipartArgumentResolveController {
+
+    @RequestMapping("multipartForm")
+    public String multipartForm() {
+        return "myMultipartFormView";
+    }
+
+    @RequestMapping("multipartFormSubmit")
+    public String multipartFormSubmit(@RequestPart int firstName, @RequestPart String lastName, @RequestPart MultipartFile photo) {
+        return "myFormAgreedView";
+    }
+
+}
---
+++ src/main/java/com/mxixm/spring/boot/chapter08/ThemeConfig.java
@@ -0,0 +1,15 @@
+package com.mxixm.spring.boot.chapter08;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.ThemeResolver;
+import org.springframework.web.servlet.theme.SessionThemeResolver;
+
+//@Configuration
+public class ThemeConfig {
+
+    @Bean
+    public ThemeResolver themeResolver() {
+        return new SessionThemeResolver();
+    }
+
+}
