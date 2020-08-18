cddf1bf340ed79264e8e35b464eb931b8f620faf
zhangx511 <zhangx511@chinaunicom.cn>
2020-08-18 18:12:24 +0800

    Spring Boot实例

--- src/main/java/com/mxixm/spring/boot/chapter02/view/RedirectViewController.java
+++ src/main/java/com/mxixm/spring/boot/chapter02/view/RedirectViewController.java
@@ -5,22 +5,38 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+/**
+ * @author zhangx511
+ */
 @Controller
 public class RedirectViewController {
 
-    // 访问的目标请求
+    /**
+     * 访问的目标请求
+     */
     @RequestMapping("/redirectView")
     public String redirectView(RedirectAttributes model) {
-        // 设置重定向前的模型属性
+
+        /**
+         * 设置重定向前的模型属性
+         */
         model.addFlashAttribute("first", "重定向前属性");
-        // 返回重定向视图，转发目标是forwardTargetView
+
+        /**
+         * 返回重定向视图，转发目标是forwardTargetView
+         */
         return "redirect:redirectTargetView";
     }
 
-    // 重定向的目标请求
+    /**
+     * 重定向的目标请求
+     */
     @RequestMapping("/redirectTargetView")
     public String redirectTargetView(Model model) {
-        // 此时的model已经有了重定向前的属性了
+
+        /**
+         * 此时的model已经有了重定向前的属性了
+         */
         model.addAttribute("second", "重定向后属性");
         return "viewView";
     }
