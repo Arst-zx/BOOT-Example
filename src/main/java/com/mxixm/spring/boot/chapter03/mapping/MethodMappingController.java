c99dd0834a13903a046e2a552f7b9a9cdf5f379e
zhangx511 <zhangx511@chinaunicom.cn>
2020-08-19 17:44:15 +0800

    Spring Boot实例 第三章 控制器的详细使用

--- src/main/java/com/mxixm/spring/boot/chapter03/mapping/MethodMappingController.java
+++ src/main/java/com/mxixm/spring/boot/chapter03/mapping/MethodMappingController.java
@@ -1,15 +1,20 @@
 package com.mxixm.spring.boot.chapter03.mapping;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+/**
+ * @author zhangx511
+ */
 @Controller
 @RequestMapping
 public class MethodMappingController {
 
     @RequestMapping(path = "path1", method = RequestMethod.GET)
-    public String path1() {
+    public String path1(Model model) {
+        model.addAttribute("name", "name");
         return "index";
     }
 
