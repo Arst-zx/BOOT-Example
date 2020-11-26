95f0d3993d4d7dcf35a7c3483f12a672b3343f34
zhangx511 <zhangx511@chinaunicom.cn>
2020-11-26 11:42:08 +0800

    算法学习

--- src/main/java/com/mxixm/spring/boot/chapter04/MyCorsController.java
+++ src/main/java/com/mxixm/spring/boot/chapter04/MyCorsController.java
@@ -5,11 +5,16 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * @author zhangx511
+ */
 @RestController
 @CrossOrigin
 public class MyCorsController {
 
-    // 只允许Origin是https://www.baidu.com的跨域请求
+    /**
+     * 只允许Origin是https://www.baidu.com的跨域请求
+     */
     @CrossOrigin(origins = "https://www.baidu.com")
     @GetMapping("myCors")
     public String myCors() {
